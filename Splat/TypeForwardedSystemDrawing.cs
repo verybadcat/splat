2ca82334f7465fb161d56c486122f6b8d31d0405
William Jockusch <jockusch@gmail.com>
2016-06-24 16:56:34 +0300

    corrected fix

--- Splat/TypeForwardedSystemDrawing.cs
+++ Splat/TypeForwardedSystemDrawing.cs
@@ -2,11 +2,6 @@
 using System.Drawing;
 using System.Runtime.CompilerServices;
 
-#if !UIKIT
-[assembly: TypeForwardedTo(typeof(Color))]
-[assembly: TypeForwardedTo(typeof(KnownColor))]
-#endif
-
 [assembly: TypeForwardedTo(typeof(Point))]
 [assembly: TypeForwardedTo(typeof(PointF))]
 [assembly: TypeForwardedTo(typeof(Rectangle))]
